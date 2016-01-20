f5589c6742d8222782c585e9c1ac4a5313107eac
Jorge Quilcate <quilcate.jorge@gmail.com>
2016-01-20 07:53:05 -0500

    move to ol7

---
+++ scripts/ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+# install epel repository
+rpm -ivh $EPEL_REPO
+
+# Configure sudo
+yum -y install sudo
+sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers
+
+# install ansible
+yum -y install PyYAML python-jinja2 python-httplib2 python-keyczar python-paramiko python-setuptools git python-pip gcc openssl-devel python-devel
+pip install --upgrade pip
+pip install ansible
