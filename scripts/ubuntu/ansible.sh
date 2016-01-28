7d78539230873f1dbe574ec17fcf1308aa3c7a1a
Jorge Quilcate <quilcate.jorge@gmail.com>
2016-01-28 15:11:59 -0500

    vars included

--- scripts/ubuntu/ansible.sh
+++ scripts/ubuntu/ansible.sh
@@ -4,7 +4,8 @@
 apt-get -y install sudo
 sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers
 
-apt-get -y install python-pip python-dev build-essential 
 # install ansible
+apt-get -y update
+apt-get install -y python-yaml python-jinja2 python-httplib2 python-keyczar python-paramiko python-setuptools python-pkg-resources git python-pip
 pip install --upgrade pip
 pip install ansible
