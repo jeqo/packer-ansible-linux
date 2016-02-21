f1bda0bc1896759c9d59f15da311703b55de10d0
Jorge Quilcate <quilcate.jorge@gmail.com>
2016-02-21 13:18:13 -0500

    fix ansible script on ubuntu

--- scripts/ubuntu/ansible.sh
+++ scripts/ubuntu/ansible.sh
@@ -8,9 +8,9 @@ sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers
 apt-get -y update
 apt-get install -y python-yaml python-jinja2 python-httplib2 python-keyczar python-paramiko python-setuptools python-pkg-resources git python-pip
 
-apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
 
-apt-get install libffi-dev libssl-dev
+apt-get install -y libffi-dev libssl-dev
 
 pip install --upgrade pip
 pip install ansible
